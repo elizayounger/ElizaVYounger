82634511af6b143219175f31b23372f117cb406a
elizayounger <elizayounger@hotmail.co.uk>
2025-05-16 13:00:14 +0100

    initial grid setup and bootstrap config

---
+++ task/vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elizayounger/it_career_switch_task',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '86cc8c4642467d320d5ba48735e728bf0f36985c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elizayounger/it_career_switch_task' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '86cc8c4642467d320d5ba48735e728bf0f36985c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.3.6',
+            'version' => '5.3.6.0',
+            'reference' => 'f849680d16a9695c9a6c9c062d6cff55ddcf071e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.3.6',
+            ),
+        ),
+    ),
+);
